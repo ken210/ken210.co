6dc3afa404797b3cb4a7a3429500662655961641
Ken Rosaka <ken@agenciasalve.com.br>
2012-06-05 19:24:18 -0300

    profile

--- html/wp-content/themes/ken210/index.php
+++ html/wp-content/themes/ken210/index.php
@@ -3,6 +3,9 @@
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<?php
+	$theme_url = get_bloginfo('stylesheet_directory');
+?>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -15,15 +18,43 @@
 
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
-	<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/modernizr-2.5.3.min.js"></script>
+	<script src="<?= $theme_url; ?>/js/libs/modernizr-2.5.3.min.js"></script>
 </head>
 <body>
 	<div id="wrapper">
 		<div class="content">
-			<header>
+			<header id="header">
 				<h1>ken210<br /><span>&amp;co</span></h1>
-				<h2>Front-end badass</h2>
+				<h3><span>Front-end</span> badass</h3>
 			</header>
+			
+			<header id="tinyHeader">
+			</header>
+
+			<section id="profile">
+				<h2>Profile</h2>
+				<img src="<?= $theme_url; ?>/img/zePequeno.jpg" alt="Little Dice my ass!" class="polaroid" />
+				<ul>
+					<li>
+						Graduated in Advertising at Mackezie University
+					</li>
+					<li>
+						<a href="#experience">10 years making websites</a>
+					</li>
+					<li>
+						<a href="#skills">Lots of Tech Skills</a>
+					</li>
+					<li>
+						Team Play
+						<ul>
+							<li>Good practices</li>
+							<li>File organization freak</li>
+							<li>Versatile hardworker dude</li>
+						</ul>
+					</li>
+				</ul>
+			</section>
+
 			<div role="main">
 			</div>
 			<footer>
@@ -32,12 +63,13 @@
 		</div>
 	</div>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+<script>
+	window.jQuery || document.write('<script src="<?= $theme_url; ?>/js/libs/jquery-1.7.2.min.js"><\/script>');
+</script>
 
 <!-- scripts concatenated and minified via ant build script-->
-<script src="<?php bloginfo('stylesheet_directory'); ?>/js/plugins.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
+<script src="<?= $theme_url; ?>/js/plugins.js"></script>
+<script src="<?= $theme_url; ?>/js/script.js"></script>
 <!-- end scripts-->
 
 <script>
